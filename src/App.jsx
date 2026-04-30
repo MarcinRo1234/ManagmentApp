4142cbf09e0fd325c07c62eff90b2d8ef48c55c8
Marcin Rogulski <Marcinos.Rogulski@gmail.com>
2026-04-30 16:37:26 +0200

    dodanie ref z formualarza

--- src/App.jsx
+++ src/App.jsx
@@ -11,10 +11,11 @@ function App() {
     projects: []
 
   });
+  
   let content;
 
   if(projectsState.selectedProjectId === null){
-    content = <NewProject />
+    content = <NewProject onAdd={handleAddProject}/>
   } else if (projectsState.selectedProjectId === undefined){
     content = <NoProjectSelected onStartAddProject={handleStartAddProject} />
   }
@@ -27,6 +28,19 @@ function App() {
       }
     })
   }
+  function handleAddProject(projectData) {
+    setProjectsState(prevState => {
+      const newProject = {
+        ...projectData,
+        id: Math.random()
+      }
+      return {
+        ...prevState,
+        projects: [...prevState.projects, newProject]
+      }
+    })
+  }
+  console.log(projectsState);
   return (
     <main className="h-screen my-8 flex gap-8">
       <Aside onStartAddProject={handleStartAddProject} />
