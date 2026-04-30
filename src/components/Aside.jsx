12fc4033da72b153b1f3e58cb7a1f0cd6148aafb
Marcin Rogulski <Marcinos.Rogulski@gmail.com>
2026-04-30 20:49:18 +0200

    dodanie listy do aside, dodanie zapisania forma

--- src/components/Aside.jsx
+++ src/components/Aside.jsx
@@ -1,19 +1,23 @@
 import Button from "./Button";
 
-
-export default function Aside({onStartAddProject}) {
-
+export default function Aside({ onStartAddProject, projects }) {
   return (
-
-      <aside className="w-1/3 px-8 py-16 bg-stone-900 text-stone-50 md:w-72 rounded-r-xl">
-        <h2 className="mb-8 font-bold uppercase md:text-xl text-stone-200">
-          your projects
-        </h2>
-        <div>
-          <Button onClick={onStartAddProject}>New project</Button>
-        </div>
-        <ul className="mt-8"></ul>
-      </aside>
-  )
+    <aside className="w-1/3 px-8 py-16 bg-stone-900 text-stone-50 md:w-72 rounded-r-xl">
+      <h2 className="mb-8 font-bold uppercase md:text-xl text-stone-200">
+        your projects
+      </h2>
+      <div>
+        <Button onClick={onStartAddProject}>New project</Button>
+      </div>
+      <ul className="mt-8">
+        {projects.map((project) => (
+          <li key={project.id}>
+            <button className="w-full text-left px-2 py-1 rounded-sm my-1 text-stone-400 hove:text-stone-200 hover:bg-stone-800">
+              {project.title}
+            </button>
+          </li>
+        ))}
+      </ul>
+    </aside>
+  );
 }
-
